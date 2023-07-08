172024baea1033031a0cccc6fe7f016d2934af91
Vladimir Lalabekyan <davidvovchik@gmail.com>
2023-07-08 16:08:44 +0300

    commit

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const search = document.getElementById('search');
 const post = document.getElementById('post');
 const comments = document.getElementById('comments');
 const hide = document.getElementById('hide');
-const comPost = document.getElementById('comPost');
-const comPost1 = document.getElementById('comPost1');
-const comPost2 = document.getElementById('comPost2');
-const comPost3 = document.getElementById('comPost3');
-const comPost4 = document.getElementById('comPost4');
+const messages = document.getElementById('messages');
+
 
 search.addEventListener('change', (event) => {
     const valueInput1 = event.target.value;
@@ -18,61 +15,63 @@ search.addEventListener('change', (event) => {
     } else {
         fetch(`https://jsonplaceholder.typicode.com/posts/${valueInput}`)
             .then(response => response.json())
-            .then(json => {
+            .then(person => {
                 post.style.display = 'block';
                 comments.style.display = 'block';
 
                 while (post.firstChild) {
                     post.firstChild.remove();
-                };
+                }
 
-                for (const key in json) {
-                    const field = document.createElement('p');
-                    const strongKey = document.createElement('strong');
-                    const value = document.createTextNode(`${key}: ${json[key]}`);
-                    strongKey.appendChild(document.createTextNode(key));
-                    field.appendChild(strongKey);
-                    field.appendChild(document.createTextNode(`: ${json[key]}`));
-                    post.appendChild(field);
-                };
+                const a = Object.entries(person);
+                a.map(([key, value]) => {
+                    if ([key, value]) {
+                        const p = document.createElement('p');
+                        p.setAttribute('id', 'userPost');
+                        post.append(p);
+                        p.textContent = (`${key}: ${value}`);
+                    };
+                });
 
                 hide.style.display = 'block';
 
-                return json;
+                return person;
             })
             .catch(error => console.log(error));
-
-        hide.addEventListener('click', () => {
-            post.style.display = 'none';
-            comments.style.display = 'none';
-            hide.style.display = 'none';
-            comPost.style.display = 'none';
-            comPost1.style.display = 'none';
-            comPost2.style.display = 'none';
-            comPost3.style.display = 'none';
-            comPost4.style.display = 'none';
-        });
 
         comments.addEventListener('click', () => {
             fetch(`https://jsonplaceholder.typicode.com/comments?postId=${valueInput}`)
                 .then(response => response.json())
-                .then(json => {
-                    comPost.style.display = 'block';
-                    comPost1.style.display = 'block';
-                    comPost2.style.display = 'block';
-                    comPost3.style.display = 'block';
-                    comPost4.style.display = 'block';
-                    document.getElementById('search').value = '';
-                    try {
-                        comPost.innerHTML = (`id: ${json[0].id} <br>name: ${json[0].name}<br>email: ${json[0].email}<br>body: ${json[0].body}`);
-                        comPost1.innerHTML = (`id: ${json[1].id}<br>name: ${json[1].name}<br>email: ${json[1].email}<br>body: ${json[1].body}`);
-                        comPost2.innerHTML = (`id: ${json[2].id}<br>name: ${json[2].name}<br>email: ${json[2].email}<br>body: ${json[2].body}`);
-                        comPost3.innerHTML = (`id: ${json[3].id}<br>name: ${json[3].name}<br>email: ${json[3].email}<br>body: ${json[3].body}`);
-                        comPost4.innerHTML = (`id: ${json[4].id}<br>name: ${json[4].name}<br>email: ${json[4].email}<br>body: ${json[4].body}`);
-                    } catch (error) {
-                        console.error('Случилась ошибка при парсинге JSON:', error);
+                .then(users => {
+
+                    while (messages.firstChild) {
+                        messages.firstChild.remove();
+                    }
+
+                    for (let i = 0; i < users.length; i++) {
+                        const arr = Object.entries(users[i]);
+                        arr.map(([key, value]) => {
+                            if ([key, value]) {
+                                const letter = document.createElement('p');
+                                letter.setAttribute('id', `${[i]}`);
+                                letter.style.margin = '0';
+                                const br = document.createElement('br');
+                                messages.append(letter);
+                                letter.textContent = (`${key}: ${value}`);
+                                if (`${key}` === 'body') {
+                                    messages.append(br);
+                                };
+
+                                hide.addEventListener('click', () => {
+                                    post.style.display = 'none';
+                                    comments.style.display = 'none';
+                                    hide.style.display = 'none';
+                                    letter.style.display = 'none';
+                                    document.getElementById('search').value = '';
+                                });
+                            };
+                        });
                     };
-                    return json;
                 })
                 .catch(error => console.log(error));
         });
